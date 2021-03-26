5290775bd5f78bf93e1f8c5528358930638b97a8
ottofabian <fabi.otto@t-online.de>
2021-03-26 15:32:57 +0100

    added balancing task

--- example.py
+++ example.py
@@ -4,17 +4,19 @@ import gym
 
 if __name__ == '__main__':
 
-    env = gym.make('alr_envs:ALRReacher-v0')
     # env = gym.make('alr_envs:SimpleReacher-v0')
+    env = gym.make('alr_envs:ALRReacher-v0')
     # env = gym.make('alr_envs:ALRReacher7-v0')
+    env = gym.make('alr_envs:Balancing-v0')
     state = env.reset()
 
     for i in range(10000):
         state, reward, done, info = env.step(env.action_space.sample())
+        print(reward)
         if i % 1 == 0:
             env.render()
 
-        # if done:
-        state = env.reset()
+        if done:
+            state = env.reset()
 
-        time.sleep(0.5)
+        # time.sleep(0.5)
