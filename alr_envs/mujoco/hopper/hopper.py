5addcb9b6ada850d425e9f4198e7c57a25e82819
Alexander Da Silva Schorn <dasilvaschorn@fzi.de>
2021-05-18 10:47:18 +0200

    Setup ALR hopper env

--- alr_envs/mujoco/hopper/hopper.py
+++ alr_envs/mujoco/hopper/hopper.py
@@ -2,38 +2,29 @@ from gym import utils
 import gym
 import numpy as np
 from alr_envs.mujoco import alr_mujoco_env
-from gym.envs.mujoco import hopper
-
+from gym.envs.mujoco import HopperEnv
 
 from stable_baselines3 import PPO
 
-class alr_hopper(hopper):
+class ALRHopperEnv(HopperEnv):
+    metadata = {'render.modes': ['human']}
+
+    def __init__(self):
+        super().__init__()
+
     def step(self, a):
         heightbefore = self.sim.data.qpos[1]
         self.do_simulation(a, self.frame_skip)
         pos, height, ang = self.sim.data.qpos[0:3]
         alive_bonus = 1.0
         reward = (height - heightbefore) / self.dt
-        reward += alive_bonus
+        #reward += alive_bonus
         reward -= 1e-3 * np.square(a).sum()
         s = self.state_vector()
         done = not (np.isfinite(s).all() and (np.abs(s[2:]) < 100).all() and
                     (height > .7) and (abs(ang) < .2))
         ob = self._get_obs()
         return ob, reward, done, {}
-
-        # posbefore = self.sim.data.qpos[0]
-        # self.do_simulation(a, self.frame_skip)
-        # posafter, height, ang = self.sim.data.qpos[0:3]
-        # alive_bonus = 1.0
-        # reward = (posafter - posbefore) / self.dt
-        # reward += alive_bonus
-        # reward -= 1e-3 * np.square(a).sum()
-        # s = self.state_vector()
-        # done = not (np.isfinite(s).all() and (np.abs(s[2:]) < 100).all() and
-        #             (height > .7) and (abs(ang) < .2))
-        # ob = self._get_obs()
-        # return ob, reward, done, {}
 
 
 def example_hopper():
@@ -58,10 +49,10 @@ def example_ppo(env):
     # env = DummyVecEnv([lambda: env])
     # env = VecNormalize(env, norm_obs=True, norm_reward=False, clip_obs=10.)
     model = PPO("MlpPolicy", env, verbose=1)
-    model.learn(total_timesteps=10000)
+    model.learn(total_timesteps=100000)
 
     obs = env.reset()
-    for i in range(1000):
+    for i in range(10000):
         action, _states = model.predict(obs, deterministic=True)
         obs, reward, done, info = env.step(action)
         env.render()
@@ -74,6 +65,7 @@ def example_ppo(env):
 
 if __name__ == "__main__":
     #example_hopper()
-    env = gym.make("Hopper-v2")
+    # env = gym.make("Hopper-v2")
+    env = gym.make("ALRHopper-v0")
     example_ppo(env)
 
