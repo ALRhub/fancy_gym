b9b0151c7c970d61b279fe4fb1bcfb6d5b5f0520
Alexander Da Silva Schorn <dasilvaschorn@fzi.de>
2021-05-18 12:32:04 +0200

    Save hopper training

--- alr_envs/mujoco/hopper/hopper.py
+++ alr_envs/mujoco/hopper/hopper.py
@@ -3,14 +3,10 @@ import gym
 import numpy as np
 from alr_envs.mujoco import alr_mujoco_env
 from gym.envs.mujoco import HopperEnv
+from t5.algorithm import CMA
 
 from stable_baselines3 import PPO
 from stable_baselines3 import SAC
-
-
-class alr_hopper(HopperEnv):
-    def __init__():
-        super.__init__()
 
 class ALRHopperEnv(HopperEnv):
     metadata = {'render.modes': ['human']}
@@ -21,16 +17,18 @@ class ALRHopperEnv(HopperEnv):
     def step(self, a):
         heightbefore = self.sim.data.qpos[1]
         self.do_simulation(a, self.frame_skip)
-        pos, height, ang = self.sim.data.qpos[0:3]
-        alive_bonus = 1.0
+        pos, height, angle = self.sim.data.qpos[0:3]
+        alive_bonus = 0.1
         reward = (height - heightbefore) / self.dt
-        #reward += alive_bonus
+        reward += alive_bonus
         reward -= 1e-3 * np.square(a).sum()
         s = self.state_vector()
-        done = not (np.isfinite(s).all() and (np.abs(s[2:]) < 100).all() and
-                    (height > .7) and (abs(ang) < .2))
-        ob = self._get_obs()
-        return ob, reward, done, {}
+        done = not (np.isfinite(s).all() and (np.abs(s[2:]) < 100).all()
+                    and (height > .7))
+        # done = not (np.isfinite(s).all() and (np.abs(s[2:]) < 100).all() and
+        #             (height > .7) and (abs(angle) < .2))
+        obs = self._get_obs()
+        return obs, reward, done, {}
 
 
 def example_hopper():
@@ -67,13 +65,14 @@ def example_ppo(env):
     env.close()
 
 
-def example_sac(env):
+def train_sac(env):
     model = SAC("MlpPolicy", env, verbose=1)
     model.learn(total_timesteps=10000, log_interval=4)
     model.save("sac_hopper")
+    # del model # remove to demonstrate saving and loading
 
-    del model # remove to demonstrate saving and loading
 
+def load_sac(env):
     model = SAC.load("sac_hopper")
 
     obs = env.reset()
@@ -85,13 +84,10 @@ def example_sac(env):
             obs = env.reset()
 
 
-
 if __name__ == "__main__":
-    #example_hopper()
+    # example_hopper()
     # env = gym.make("Hopper-v2")
     env = gym.make("ALRHopper-v0")
-    example_ppo(env)
-    #example_sac(env)
-
-    
-
+    train_sac(env)
+    load_sac(env)
+    # example_sac(env)
