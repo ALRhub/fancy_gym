c031b9ecdd052b9b3196b2b242086addf56b3919
Felix Köhler <koehlerfelix@arcor.de>
2021-05-18 09:25:28 +0200

    changed step method form hopper

--- alr_envs/mujoco/hopper/hopper.py
+++ alr_envs/mujoco/hopper/hopper.py
@@ -2,14 +2,45 @@ from gym import utils
 import gym
 import numpy as np
 from alr_envs.mujoco import alr_mujoco_env
+from gym.envs.mujoco import hopper
 
 class hopper(alr_mujoco_env.AlrMujocoEnv):
     def __init__(self):
         alr_mujoco_env.AlrMujocoEnv.__init__(self)
 
 
+class alr_hopper(hopper):
+    def step(self, a):
+        heightbefore = self.sim.data.qpos[1]
+        self.do_simulation(a, self.frame_skip)
+        pos, height, ang = self.sim.data.qpos[0:3]
+        alive_bonus = 1.0
+        reward = (height - heightbefore) / self.dt
+        reward += alive_bonus
+        reward -= 1e-3 * np.square(a).sum()
+        s = self.state_vector()
+        done = not (np.isfinite(s).all() and (np.abs(s[2:]) < 100).all() and
+                    (height > .7) and (abs(ang) < .2))
+        ob = self._get_obs()
+        return ob, reward, done, {}
+
+        # posbefore = self.sim.data.qpos[0]
+        # self.do_simulation(a, self.frame_skip)
+        # posafter, height, ang = self.sim.data.qpos[0:3]
+        # alive_bonus = 1.0
+        # reward = (posafter - posbefore) / self.dt
+        # reward += alive_bonus
+        # reward -= 1e-3 * np.square(a).sum()
+        # s = self.state_vector()
+        # done = not (np.isfinite(s).all() and (np.abs(s[2:]) < 100).all() and
+        #             (height > .7) and (abs(ang) < .2))
+        # ob = self._get_obs()
+        # return ob, reward, done, {}
+
+
 def example_hopper():
     env = gym.make('Hopper-v2')
+    env = hopper
     rewards = 0
     obs = env.reset()
 
@@ -48,4 +79,9 @@ if __name__ == "__main__":
     #         break
 
     # env.close()
-    example_hopper()
+    # example_hopper()
+    env = gym.make("Hopper-v2")
+    print(env.action_space)
+    print(env.action_space.shape)
+    print(env.observation_space)
+    print(env.observation_space.shape)
