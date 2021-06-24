e250ec491e03d87174d9bcd85112a22882fadef3
ottofabian <fabi.otto@t-online.de>
2021-06-24 16:51:38 +0200

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,21 @@
 from setuptools import setup
 
-setup(name='alr_envs',
-      version='0.0.1',
-      install_requires=['gym',
-                        'PyQt5',
-                        'matplotlib',
-                        'mp_lib @ git+https://git@github.com/maxhuettenrauch/mp_lib@master#egg=mp_lib',
-                        'mujoco_py'],  # And any other dependencies foo needs
-      )
+setup(
+    name='alr_envs',
+    version='0.0.1',
+    packages=['alr_envs', 'alr_envs.classic_control', 'alr_envs.mujoco', 'alr_envs.stochastic_search',
+              'alr_envs.utils'],
+    install_requires=[
+        'gym',
+        'PyQt5',
+        'matplotlib',
+        'mp_env_api @ git+ssh://git@github.com/ALRhub/motion_primitive_env_api.git',
+        'mujoco_py'
+    ],
+
+    url='https://github.com/ALRhub/alr_envs/',
+    license='MIT',
+    author='Fabian Otto, Marcel Sandermann, Maximilian Huettenrauch',
+    author_email='',
+    description='Custom Gym environments for various (robotics) simple_reacher.'
+)
