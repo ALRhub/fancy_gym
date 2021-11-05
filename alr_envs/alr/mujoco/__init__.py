598a7d5512c0985aab8411daa0130b4906e6275d
Marcel <unsux@student.kit.edu>
2021-11-05 11:04:04 +0100

    Working Beerpong and TableTennis with MPWrapper

--- alr_envs/alr/mujoco/__init__.py
+++ alr_envs/alr/mujoco/__init__.py
@@ -2,3 +2,5 @@ from .reacher.alr_reacher import ALRReacherEnv
 from .reacher.balancing import BalancingEnv
 from .ball_in_a_cup.ball_in_a_cup import ALRBallInACupEnv
 from .ball_in_a_cup.biac_pd import ALRBallInACupPDEnv
+from .table_tennis.tt_gym import TT_Env_Gym
+from .beerpong.beerpong import ALRBeerBongEnv
