2283f4416cc86936114a613c70164f233139012d
ottofabian <fabian.otto@partner.kit.edu>
2022-07-14 09:45:35 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 # Environment-specific dependencies for dmc and metaworld
 extras = {
-    "dmc": ["dm_control==1.0.1"],
+    "dmc": ["dm_control>=1.0.1"],
     "metaworld": ["metaworld @ git+https://github.com/rlworkgroup/metaworld.git@master#egg=metaworld",
                   'mujoco-py<2.2,>=2.1'],
 }
@@ -14,11 +14,11 @@ all_groups = set(extras.keys())
 extras["all"] = list(set(itertools.chain.from_iterable(map(lambda group: extras[group], all_groups))))
 
 setup(
-    author='Fabian Otto, Onur Celik, Marcel Sandermann, Maximilian Huettenrauch',
-    name='simple_gym',
-    version='0.1',
+    author='Fabian Otto, Onur Celik',
+    name='fancy_gym',
+    version='0.2',
     classifiers=[
-        # Python 3.6 is minimally supported (only with basic gym environments and API)
+        # Python 3.7 is minimally supported
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
@@ -29,9 +29,9 @@ setup(
     install_requires=[
         'gym>=0.24.0',
         'mujoco==2.2.0',
+        'mp_pytorch @ git+https://github.com/ALRhub/MP_PyTorch.git@master'
     ],
     packages=[package for package in find_packages() if package.startswith("fancy_gym")],
-    # packages=['fancy_gym', 'fancy_gym.envs', 'fancy_gym.open_ai', 'fancy_gym.dmc', 'fancy_gym.meta', 'fancy_gym.utils'],
     package_data={
         "fancy_gym": [
             "envs/mujoco/*/assets/*.xml",
