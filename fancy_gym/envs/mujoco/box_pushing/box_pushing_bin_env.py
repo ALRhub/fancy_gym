da2b1d46bd4395af069a931a3f244408b097415f
Dhimiter Pikuli <pid6rt@bosch.com>
2023-03-28 02:31:20 +0200

    add funcion to return edge of top stack box in -x direction, limit box spawn

--- fancy_gym/envs/mujoco/box_pushing/box_pushing_bin_env.py
+++ fancy_gym/envs/mujoco/box_pushing/box_pushing_bin_env.py
@@ -18,7 +18,7 @@ PUSH_DISTANCE = 0.03  # 3cm ~ 1 / 3 of box sizes
 
 # Need to set by hand depending on the environment config in the xml files
 MAX_NUM_BOXES = 10
-BOX_POS_BOUND = np.array([[0.4, -0.3, 0.06], [0.8, 0.3, 0.45]])
+BOX_POS_BOUND = np.array([[0.54, -0.3, 0.06], [0.8, 0.3, 0.45]])
 BIN_SIZE = 0.2
 NUM_BINS = 3
 # (ahead, lean 45 back, ahead, lean 225 front, ahead, look down, camera front)
@@ -241,6 +241,11 @@ class BoxPushingBin(MujocoEnv, utils.EzPickle):
 
     def robot_state(self):
         return self.data.qpos[:7].copy()  # joint position
+
+    def pos_behind_box(self):
+        box_pos = np.array(self.data.body(self.boxes[0]).xpos.copy()[:3])
+        box_pos[0] -= 0.08
+        return box_pos
 
     def get_obs(self):
         return self._get_obs()
