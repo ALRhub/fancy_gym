61ec5083307ef0e377a3885a40b0c2f58a669848
zeqi_jin <zeqi.jin@student.kit.edu>
2023-04-19 17:55:40 +0200

    change plot traj function

--- fancy_gym/examples/examples_air_hockey.py
+++ fancy_gym/examples/examples_air_hockey.py
@@ -10,31 +10,32 @@ import scipy
 
 
 def plot_trajs(position, velocity, jerk):
+    dt = 0.02
     pos = position
-    vel = np.diff(pos, n=1, axis=0) / 0.02
-    acc = np.diff(vel, n=1, axis=0) / 0.02
-    jer = np.diff(acc, n=1, axis=0) / 0.02
+    vel = np.diff(pos, n=1, axis=0) / dt
+    acc = np.diff(vel, n=1, axis=0) / dt
+    jer = np.diff(acc, n=1, axis=0) / dt
 
-    pos = pos[:-3]
-    vel = vel[:-2]
-    acc = acc[:-1]
-    jer = jer
-    jerk = jerk[:-3]
+    pos = position[:-9]  # 141 steps for 2801 interp steps
+    vel = velocity[:-9]
+    acc = acc[:-7]
+    jer = jer[:-6]
+    jerk = jerk[:-9]
 
     # interp pos
-    tf = 0.02
+    tf = dt
     prev_pos = position[0]
     prev_vel = velocity[0]
     prev_acc = np.array([0, 0, 0])
     interp_pos = [prev_pos]
     interp_vel = [prev_vel]
     interp_acc = [prev_acc]
-    for i in range(position.shape[0] - 1):
+    for i in range(position.shape[0] - 10):
         coef = np.array([[1, 0, 0, 0], [1, tf, tf ** 2, tf ** 3], [0, 1, 0, 0], [0, 1, 2 * tf, 3 * tf ** 2]])
         reg = np.array([[1e-4, 0, 0, 0], [0, 1e-4, 0, 0], [0, 0, 1e-4, 0], [0, 0, 0, 0.001]])
-        coef_reg = coef + reg
+        # coef = coef + reg
         results = np.vstack([prev_pos, position[i+1], prev_vel, velocity[i+1]])
-        A = scipy.linalg.block_diag(*[coef_reg] * 3)
+        A = scipy.linalg.block_diag(*[coef] * 3)
         y = results.reshape(-1, order='F')
         weights = np.linalg.solve(A, y).reshape(3, 4)
         weights_d = np.polynomial.polynomial.polyder(weights, axis=1)
@@ -52,7 +53,7 @@ def plot_trajs(position, velocity, jerk):
             interp_vel.append(qd)
             interp_acc.append(qdd)
 
-    step = np.linspace(0, 3, pos.shape[0])
+    step = np.linspace(dt, dt * pos.shape[0], pos.shape[0])
     constr_j_pos = [[-2.9, +2.9], [-1.8, +1.8], [-2.0, +2.0]]
     constr_j_vel = [[-1.5, +1.5], [-1.5, +1.5], [-2.0, +2.0]]
     constr_j_jerk = [[0, 1e4]] * 3
@@ -60,36 +61,41 @@ def plot_trajs(position, velocity, jerk):
     interp_pos = np.array(interp_pos)
     interp_vel = np.array(interp_vel)
     interp_acc = np.array(interp_acc)
-    interp_step = np.linspace(0, 3, interp_pos.shape[0])
-    # plt.subplots(3, 2)
+    interp_step = np.linspace(dt, dt * pos.shape[0], interp_pos.shape[0])
+
+    s = 130
+    ss = 20 * s
+    e = 141
+    ee = 20 * (e - 1) + 1
     for k in range(3):
         plt.subplot(3, 5, 5 * k + 1)
-        plt.plot(step[:11], pos[:11, k])
-        plt.plot(interp_step[:201], interp_pos[:201, k])
-        # plt.plot(step, pos[:, k])
-        # plt.plot(interp_step, interp_pos[:, k])
-        # plt.hlines(constr_j_pos[k], xmin=step[0], xmax=step[-1])
+        if k == 0:
+            plt.title("mp_pos vs. interp_pos")
+        plt.plot(step[s:e], pos[s:e, k])
+        plt.plot(interp_step[ss:ee], interp_pos[ss:ee, k])
+        # plt.hlines(constr_j_pos[k], xmin=step[0], xmax=step[-1], colors="r")
 
         plt.subplot(3, 5, 5 * k + 2)
-        # plt.plot(step[:11], vel[:11, k])
-        plt.plot(step[:11], velocity[:11, k])
-        plt.plot(interp_step[:201], interp_vel[:201, k])
-        # plt.plot(step, velocity[:-3, k])
-        # plt.plot(interp_step, interp_vel[:, k])
-        # plt.hlines(constr_j_vel[k], xmin=step[0], xmax=step[-1])
+        if k == 0:
+            plt.title("mp_vel vs. interp_vel")
+        plt.plot(step[s:e], vel[s:e, k])
+        plt.plot(interp_step[ss:ee], interp_vel[ss:ee, k])
+        # plt.hlines(constr_j_vel[k], xmin=step[0], xmax=step[-1], colors="r")
 
         plt.subplot(3, 5, 5 * k + 3)
-        plt.plot(step[:11], acc[:11, k])
-        plt.plot(interp_step[:201], interp_acc[:201, k])
-        # plt.plot(step, acc[:, k])
-        # plt.plot(interp_step, interp_acc[:, k])
-        # plt.plot(step[40:50], acc[40:50, k])
+        if k == 0:
+            plt.title("mp_acc vs. interp_acc")
+        plt.plot(interp_step[ss:ee], interp_acc[ss:ee, k])
+        plt.plot(step[s:e], acc[s:e, k])
 
         plt.subplot(3, 5, 5 * k + 4)
+        if k == 0:
+            plt.title("mp_jerk")
         plt.plot(step, jer[:, k])
-        # plt.plot(step, jerk[:, k] * 0.001)
 
         plt.subplot(3, 5, 5 * k + 5)
+        if k == 0:
+            plt.title("interp_jerk")
         plt.plot(step, jerk[:, k])
         plt.hlines(constr_j_jerk[k], xmin=step[0], xmax=step[-1], colors='r')
     plt.show()
@@ -119,11 +125,11 @@ def test_baseline(env_id="3dof-hit", seed=0, iteration=5):
 
         agent.reset()
         obs = env.reset()
-        env.render(mode="human")
+        # env.render(mode="human")
         while True:
             act = agent.draw_action(obs).reshape([-1])
             obs_, rew, done, info = env.step(act)
-            env.render(mode="human")
+            # env.render(mode="human")
 
             j_pos.append(act[:3])
             j_vel.append(act[3:])
@@ -141,7 +147,7 @@ def test_baseline(env_id="3dof-hit", seed=0, iteration=5):
                 print('constr_j_pos: ', np.sum(constrs['j_pos']))
                 print('constr_j_vel: ', np.sum(constrs['j_vel']))
                 print('constr_ee: ', np.sum(constrs['ee']))
-                # plot_trajs(np.array(j_pos), np.array(j_vel), np.array(traj_jerk))
+                plot_trajs(np.array(j_pos), np.array(j_vel), np.array(traj_jerk))
                 break
 
 
@@ -179,20 +185,22 @@ def test_env(env_id="3dof-hit", seed=0, iteration=5):
 def test_mp_env(env_id="3dof-hit-promp", seed=0, iteration=5):
     env = fancy_gym.make(env_id=env_id, seed=seed)
 
+    act_list = [np.array([+0.4668, +0.2761, +0.2246, -0.0090, -0.0328, -0.5161,
+                          -0.1360, -0.3141, -0.4803, -0.9457, -0.5832, -0.3209]),
+                np.array([+0.3490, +0.0597, +0.1681, +0.2891, -0.3729, -0.6172,
+                          -0.4291, -0.3400, -0.5428, -0.8549, -0.5452, -0.2657]),
+                np.array([+0.0796, +0.1585, +0.1650, +0.2431, -0.5435, -0.5349,
+                          -0.3300, -0.3566, -0.5377, -1.0708, -0.5976, -0.3642])]
+
     for i in range(iteration):
         print("*"*20, i, "*"*20)
         obs = env.reset()
         # if i == 0:
         #     env.render(mode="human")
         while True:
-            # act = env.action_space.sample()
-            act = np.array([+0.4343, +0.4926, +0.3588, +0.2393, -0.0943, -0.5331,
-                            -0.3986, -0.2830, -0.5116, -0.9724, -0.7811, -0.5152])
-            act = np.array([+0.3897, +0.3356, +0.3108, +0.1727, -0.1591, -0.5672,
-                            -0.2583, -0.2497, -0.4358, -0.9179, -0.7147, -0.4880])
-            act = np.array([+0.3897, +0.3356, +0.3108, +0.1727, -0.1591, -0.5672,
-                            -0.2583, -0.2497, -0.4358, -0.9179, -0.7147, -0.4880])
+            act = env.action_space.sample()
             # act = np.hstack([act, act, act])
+            act = act_list[0]
             obs, rew, done, info = env.step(act)
 
             # plot trajs
@@ -279,7 +287,7 @@ def test_learn_mp():
 
 
 if __name__ == "__main__":
-    # test_baseline(env_id='3dof-hit-sparse', iteration=10)
+    # test_baseline(env_id='3dof-hit-sparse', iteration=1)
     # test_env(env_id="3dof-hit-sparse", iteration=10)
-    test_mp_env(env_id="3dof-hit-sparse-promp", seed=1, iteration=100)
+    test_mp_env(env_id="3dof-hit-sparse-promp", seed=1, iteration=1)
     # test_mp()
