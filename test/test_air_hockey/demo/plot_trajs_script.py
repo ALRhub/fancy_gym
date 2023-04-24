34f2da153bf17bcd381ba15c8346d80edbec87b2
zeqi_jin <zeqi.jin@student.kit.edu>
2023-04-24 15:50:53 +0200

    mp_low vs. cubic_interp

--- test/test_air_hockey/demo/plot_trajs_script.py
+++ test/test_air_hockey/demo/plot_trajs_script.py
@@ -132,6 +132,43 @@ def plot_single_dof_trajs(start_index=0, end_index=150):
     jer_low = np.abs(np.diff(acc_low, n=1, axis=0, append=np.zeros((1, 3))) / dt_low)
     time_low = np.linspace(0, 3.0, pos_low.shape[0])
 
+    # cubic polynomial interpolation traj
+    tf = dt_low
+    prev_pos = pos_low[0]
+    prev_vel = vel_low[0]
+    prev_acc = acc_low[0]
+    pos_interp = [prev_pos]
+    vel_interp = [prev_vel]
+    acc_interp = [prev_acc]
+    jer_interp = [np.zeros(3)]
+    for i in range(pos_low.shape[0] - 1):
+        coef = np.array([[1, 0, 0, 0], [1, tf, tf ** 2, tf ** 3], [0, 1, 0, 0], [0, 1, 2 * tf, 3 * tf ** 2]])
+        results = np.vstack([prev_pos, pos_low[i+1], prev_vel, vel_low[i+1]])
+        A = sp.linalg.block_diag(*[coef] * 3)
+        y = results.reshape(-1, order='F')
+        weights = np.linalg.solve(A, y).reshape(3, 4)
+        weights_d = np.polynomial.polynomial.polyder(weights, axis=1)
+        weights_dd = np.polynomial.polynomial.polyder(weights_d, axis=1)
+
+        jer_interp.append(np.abs(weights_dd[:, 1]) + np.abs(weights_dd[:, 0] - prev_acc) / dt_high)
+
+        prev_pos = np.polynomial.polynomial.polyval(tf, weights.T)
+        prev_vel = np.polynomial.polynomial.polyval(tf, weights_d.T)
+        prev_acc = np.polynomial.polynomial.polyval(tf, weights_dd.T)
+
+        for t in np.linspace(0.001, 0.02, 20):
+            q = np.polynomial.polynomial.polyval(t, weights.T)
+            qd = np.polynomial.polynomial.polyval(t, weights_d.T)
+            qdd = np.polynomial.polynomial.polyval(t, weights_dd.T)
+            pos_interp.append(q)
+            vel_interp.append(qd)
+            acc_interp.append(qdd)
+
+    pos_high = np.array(pos_interp)
+    vel_high = np.array(vel_interp)
+    acc_high = np.array(acc_interp)
+    jer_high = np.array(jer_interp)
+
     constr_j_pos = [[-2.9, +2.9], [-1.8, +1.8], [-2.0, +2.0]]
     constr_j_vel = [[-1.5, +1.5], [-1.5, +1.5], [-2.0, +2.0]]
     constr_j_jerk = [[0, 1e4]] * 3
@@ -152,7 +189,7 @@ def plot_single_dof_trajs(start_index=0, end_index=150):
         if c == 0:
             plt.title("position")
             plt.hlines(constr_j_pos[d], xmin=time_low[s], xmax=time_low[e], colors="black")
-        plt.plot(time_high[ss:ee], pos_high[ss:ee, d], color='green', label='mp_1000hz')
+        plt.plot(time_high[ss:ee], pos_high[ss:ee, d], color='green', label='cubic_interp')
         plt.plot(time_low[s:e], pos_low[s:e, d], color='blue', label='mp_50hz')
         plt.legend()
 
@@ -173,7 +210,7 @@ def plot_single_dof_trajs(start_index=0, end_index=150):
         if c == 0:
             plt.title("jerk")
             plt.hlines(constr_j_jerk[d][1], xmin=time_low[s], xmax=time_low[e], colors='black')
-        plt.plot(time_high[ss:ee], jer_high[ss:ee, d], color='green')
+        plt.plot(time_low[s:e], jer_high[s:e, d], color='green')
         plt.plot(time_low[s:e], jer_low[s:e, d], color='blue')
     plt.show()
 
