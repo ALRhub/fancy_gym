81dbdc57457c557fe04379d8b510f2ee9c52471d
Dominik Roth <dominik.roth.dev@gmail.com>
2023-12-05 18:13:20 +0100

    Updated conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,9 +1,13 @@
-project = 'Fancy Gym'
-copyright = '2023, Fabian Otto, Onur Celik'
-author = 'Fabian Otto, Onur Celik'
+# This conf.py is in large parts inspired by the oen used by stable-baselines 3
 
-release = '0.4'  # The full version, including alpha/beta/rc tags
-version = '0.4'  # The short X.Y version
+import datetime
+
+project = 'Fancy Gym'
+author = 'Fabian Otto, Onur Celik, Dominik Roth, Hongyi Zhou'
+copyright = f'2020-{datetime.date.today().year}, {author}'
+
+release = '0.2'  # The full version, including alpha/beta/rc tags
+version = '0.2'  # The short X.Y version
 
 extensions = [
     'myst_parser',
@@ -11,7 +15,10 @@ extensions = [
     'sphinx.ext.doctest',
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
-    'sphinx.ext.intersphinx',
+    # 'sphinx.ext.intersphinx',
+    'sphinx.ext.mathjax',
+    'sphinx.ext.ifconfig',
+    'sphinx.ext.viewcode',
 ]
 
 autosummary_generate = True
@@ -23,10 +30,16 @@ intersphinx_mapping = {
 
 intersphinx_disabled_domains = ['std']
 
+language = "en"
+pygments_style = "sphinx"
+
 templates_path = ['_templates']
 exclude_patterns = ['build', 'Thumbs.db', '.DS_Store']
 
-
 html_theme = 'sphinx_rtd_theme'
+html_logo = "_static/imgs/icon.svg"
 epub_show_urls = 'footnote'
 html_static_path = ['_static']
+
+def setup(app):
+    app.add_css_file("style.css")
